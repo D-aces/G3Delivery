2a0b79ed892f05b98daa4eef9f065f28e469b2a7
DemiwizAces <erdmriosnadeau@gmail.com>
2024-11-24 17:44:55 -0500

    Map dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.mapbox.navigationcore:navigation:3.5.2")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,14 +11,24 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                username = "mapbox" // Always use 'mapbox' as username
+                password = (extra["MAPBOX_DOWNLOADS_TOKEN"] as? String) ?: ""
+            }
+        }
     }
 }
 
 rootProject.name = "G3Delivery"
 include(":app")
- 
