2bb3427558b307b319b7afa6d7aa83d783ccde33
DemiwizAces <erdmriosnadeau@gmail.com>
2024-11-25 17:02:01 -0500

    ITS DONE

--- app/src/main/java/com/example/g3delivery/OrderActivity.java
+++ app/src/main/java/com/example/g3delivery/OrderActivity.java
@@ -2,6 +2,8 @@ package com.example.g3delivery;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -52,5 +54,16 @@ public class OrderActivity extends AppCompatActivity {
         orderItemsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         OrderItemAdapter adapter = new OrderItemAdapter(order.getFoodMap());
         orderItemsRecyclerView.setAdapter(adapter);
+
+        // Find the button and set its onClick listener
+        Button checkoutButton = findViewById(R.id.picture);
+        checkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to ScrollingActivity
+                Intent intent = new Intent(OrderActivity.this, CameraActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
